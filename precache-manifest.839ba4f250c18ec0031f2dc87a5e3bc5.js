b8ecb0e9f69bf1ca89bafcd0bd29b823e0c924bd
liondor <johen.sylvestre@gmail.com>
2020-07-22 08:40:21 -0400

    Updates

---
+++ precache-manifest.839ba4f250c18ec0031f2dc87a5e3bc5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba2b1bb7efaf1f4567c08cdf49974ac8",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "89f7e6c5017d9c7cbbe8",
+    "url": "/covid/static/css/main.55278e6d.chunk.css"
+  },
+  {
+    "revision": "c4a06f3df50eaa34f7b8",
+    "url": "/covid/static/js/2.efedf3ca.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid/static/js/2.efedf3ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89f7e6c5017d9c7cbbe8",
+    "url": "/covid/static/js/main.a1052fc2.chunk.js"
+  },
+  {
+    "revision": "2538616d296fec9cb619",
+    "url": "/covid/static/js/runtime-main.6f8ac736.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid/static/media/corona.d7265326.png"
+  }
+]);
