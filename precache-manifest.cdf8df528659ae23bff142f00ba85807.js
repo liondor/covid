652a3ac93bfc5d85f4bc8ba9903da55dcde8e647
liondor <johen.sylvestre@gmail.com>
2021-04-02 16:46:18 +0200

    Updates

---
+++ precache-manifest.cdf8df528659ae23bff142f00ba85807.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50805dc1975146e17b574893352fae03",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "e47c3f0597e9f77ac5a1",
+    "url": "/covid/static/css/main.55278e6d.chunk.css"
+  },
+  {
+    "revision": "c4a06f3df50eaa34f7b8",
+    "url": "/covid/static/js/2.efedf3ca.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid/static/js/2.efedf3ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e47c3f0597e9f77ac5a1",
+    "url": "/covid/static/js/main.260804e7.chunk.js"
+  },
+  {
+    "revision": "2538616d296fec9cb619",
+    "url": "/covid/static/js/runtime-main.6f8ac736.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid/static/media/corona.d7265326.png"
+  }
+]);
